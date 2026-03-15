c1605da164188710618111b19cd0265a45320baa
gauravchahar172-commits <gauravchahar172@gmail.com>
2026-03-15 08:44:28 +0530

    Implement audio playback functions
    Added audio playback functionality with playSong and togglePlay functions.

--- player.js
+++ player.js
@@ -1,1 +1,29 @@
+let audio = new Audio();
+let playing=false;
 
+function playSong(url,title,artist,cover){
+
+audio.src=url;
+audio.play();
+
+document.getElementById("title").innerText=title;
+document.getElementById("artist").innerText=artist;
+document.getElementById("cover").src=cover;
+
+document.getElementById("player").style.display="flex";
+
+playing=true;
+
+}
+
+function togglePlay(){
+
+if(playing){
+audio.pause();
+playing=false;
+}else{
+audio.play();
+playing=true;
+}
+
+}
