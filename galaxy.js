4c6e03b1238bba0ae8480ca5e50603bdf17eff5c
gauravchahar172-commits <gauravchahar172@gmail.com>
2026-03-15 08:45:13 +0530

    Initialize canvas and draw stars on it

--- galaxy.js
+++ galaxy.js
@@ -1,1 +1,30 @@
+const canvas=document.getElementById("galaxy");
+const ctx=canvas.getContext("2d");
 
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+let stars=[];
+
+for(let i=0;i<200;i++){
+stars.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+size:Math.random()*2
+});
+}
+
+function draw(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+stars.forEach(s=>{
+ctx.fillStyle="white";
+ctx.fillRect(s.x,s.y,s.size,s.size);
+});
+
+requestAnimationFrame(draw);
+
+}
+
+draw();
